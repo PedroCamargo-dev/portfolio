df8728877fa9c77412995ed0c43879c9f491815e
Pedro Camargo <pedrocamargo3008@gmail.com>
2024-01-03 19:31:13 -0300

    refactor: alter padding and add molecule skill text in organism skill

--- src/components/Organisms/Skills/index.tsx
+++ src/components/Organisms/Skills/index.tsx
@@ -1,5 +1,5 @@
 import SkillSection from "@/components/Molecules/SkillSection";
-import SkillText from "@/components/SkillText";
+import SkillText from "@/components/Molecules/SkillText";
 import {
   Backend_skill,
   Frontend_skill,
@@ -11,7 +11,7 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-20 py-20"
       style={{ transform: "scale(0.9" }}
     >
       <SkillText />
