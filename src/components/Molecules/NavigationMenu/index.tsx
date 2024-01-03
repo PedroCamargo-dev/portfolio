bd04b267340c9a30fae019f3b72ed00ae1a791b7
Pedro Camargo <pedrocamargo3008@gmail.com>
2024-01-03 16:16:08 -0300

    refactor: add molecule NavigationMenu

---
+++ src/components/Molecules/NavigationMenu/index.tsx
@@ -0,0 +1,11 @@
+import MenuLink from "@/components/Atoms/MenuLink";
+
+const NavigationMenu = () => (
+  <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+    <MenuLink href="#about-me">About me</MenuLink>
+    <MenuLink href="#skills">Skills</MenuLink>
+    <MenuLink href="#projects">Projects</MenuLink>
+  </div>
+);
+
+export default NavigationMenu;
