8a86b1a7d1c67b56befbc843f0adc0cafaea124b
Pedro Camargo <pedrocamargo3008@gmail.com>
2024-01-27 20:31:01 -0300

    refactor: add new props logo brand

--- src/components/Molecules/Brand/index.tsx
+++ src/components/Molecules/Brand/index.tsx
@@ -2,7 +2,7 @@ import Logo from "@/components/Atoms/Logo";
 
 const Brand = () => (
   <a href="#about-me" className="h-auto w-auto flex flex-row items-center">
-    <Logo />
+    <Logo width={50} height={50} className="rounded-full" />
     <span className="font-bold ml-[10px] hidden md:block text-gray-300">
       Pedro Camargo
     </span>
