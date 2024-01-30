57ca18004dd8cf5fe3fb159de5f2d215673dde45
Pedro Camargo <pedrocamargo3008@gmail.com>
2024-01-30 19:17:30 -0300

    refactor: page home and login

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,22 +1,58 @@
 "use client";
 
-import Form from "@/components/Atoms/Form";
-import { Input } from "@/components/Atoms/Input";
-import { Textarea } from "@/components/Atoms/Textarea";
-import { useCallback, useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { db, storage } from "@/utils/config/db/firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import {
   IoPencilOutline,
   IoTrashBinOutline,
   IoAddOutline,
 } from "react-icons/io5";
+import Form from "@/components/Atoms/Form";
+import { Input } from "@/components/Atoms/Input";
+import { Textarea } from "@/components/Atoms/Textarea";
+import { InputFile } from "@/components/Atoms/InputFile";
+import Image from "next/image";
+import { Spinner } from "@/components/Atoms/Spinner";
+import {
+  deleteObject,
+  getDownloadURL,
+  ref as storageRef,
+  uploadBytesResumable,
+} from "firebase/storage";
+
+type ProjectProps = {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl?: string;
+};
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
   const [editTitle, setEditTitle] = useState("");
   const [editDescription, setEditDescription] = useState("");
+  const [idProject, setIdProject] = useState("");
+  const [image, setImage] = useState<File>();
+  const [imageURL, setImageURL] = useState<string | undefined>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [percent, setPercent] = useState(0);
+  const [projects, setProjects] = useState<ProjectProps[]>([]);
 
   const handleModal = useCallback(() => {
-    setShowModal(!showModal);
+    setShowModal((prevShowModal) => !prevShowModal);
+    if (showModal) {
+      setEditTitle("");
+      setEditDescription("");
+      setIdProject("");
+      setImage(undefined);
+    }
   }, [showModal]);
 
   const handleOutsideClick = useCallback((event: MouseEvent) => {
@@ -32,11 +68,21 @@ export default function Home() {
     }
   }, []);
 
-  const handleEditClick = useCallback((title: string, description: string) => {
-    setEditTitle(title);
-    setEditDescription(description);
-    // Perform any other logic you need when the edit button is clicked
-  }, []);
+  const handleEditClick = useCallback(
+    (
+      title: string,
+      description: string,
+      id: string,
+      image: string | undefined
+    ) => {
+      setEditTitle(title);
+      setEditDescription(description);
+      setIdProject(id);
+      setImageURL(image);
+      setShowModal(true);
+    },
+    []
+  );
 
   useEffect(() => {
     document.addEventListener("mousedown", handleOutsideClick);
@@ -47,33 +93,100 @@ export default function Home() {
     };
   }, [handleOutsideClick, handleEscKey]);
 
-  const projects = [
-    {
-      id: 1,
-      title: "Husky1",
-      description: "Esse projeto vai ser o melhor app de finanças do mundo!",
-    },
-    {
-      id: 2,
-      title: "Husky2",
-      description: "Esse projeto vai ser o melhor app de finanças do mundo!",
-    },
-    {
-      id: 3,
-      title: "Husky3",
-      description: "Esse projeto vai ser o melhor app de finanças do mundo!",
-    },
-    {
-      id: 1,
-      title: "Husky4",
-      description: "Esse projeto vai ser o melhor app de finanças do mundo!",
-    },
-    {
-      id: 2,
-      title: "Husk5",
-      description: "Esse projeto vai ser o melhor app de finanças do mundo!",
-    },
-  ];
+  useEffect(() => {
+    const getProjects = async () => {
+      const querySnapshot = await getDocs(collection(db, "projects"));
+      const projectsData: ProjectProps[] = [];
+
+      await Promise.all(
+        querySnapshot.docs.map(async (doc) => {
+          const projectData = { id: doc.id, ...doc.data() } as ProjectProps;
+
+          const imageRef = storageRef(storage, `projects/${doc.id}`);
+
+          const imageUrl = await getDownloadURL(imageRef);
+
+          projectData.imageUrl = imageUrl;
+
+          projectsData.push(projectData);
+        })
+      );
+
+      setProjects(projectsData);
+    };
+
+    getProjects();
+  }, []);
+
+  const onSubmit = async (data: ProjectProps) => {
+    setIsLoading(true);
+    try {
+      if (data.id !== "") {
+        if (image) {
+          const imageRef = storageRef(storage, `projects/${data.id}`);
+          const uploadTask = uploadBytesResumable(imageRef, image as File);
+
+          uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+              const percent = Math.round(
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+              );
+              setPercent(percent);
+            },
+            (err) => console.log(err)
+          );
+        }
+
+        await updateDoc(doc(db, "projects", data.id), {
+          title: data.title,
+          description: data.description,
+        });
+      } else {
+        const response = await addDoc(collection(db, "projects"), {
+          title: data.title,
+          description: data.description,
+        });
+        if (image) {
+          const imageRef = storageRef(storage, `projects/${response.id}`);
+          const uploadTask = uploadBytesResumable(imageRef, image as File);
+
+          uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+              const percent = Math.round(
+                (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+              );
+              setPercent(percent);
+            },
+            (err) => console.log(err)
+          );
+        }
+      }
+
+      const querySnapshot = await getDocs(collection(db, "projects"));
+      const projectsData: ProjectProps[] = [];
+
+      await Promise.all(
+        querySnapshot.docs.map(async (doc) => {
+          const projectData = { id: doc.id, ...doc.data() } as ProjectProps;
+          const imageRef = storageRef(storage, `projects/${doc.id}`);
+          const imageUrl = await getDownloadURL(imageRef);
+          projectData.imageUrl = imageUrl;
+
+          projectsData.push(projectData);
+        })
+      );
+
+      setProjects(projectsData);
+
+      setIsLoading(false);
+      handleModal();
+    } catch (error: any) {
+      setIsLoading(false);
+      handleModal();
+    }
+  };
 
   return (
     <div className="flex justify-center h-full items-center p-3">
@@ -90,46 +203,62 @@ export default function Home() {
           </button>
         </div>
         <div className="w-full border-b bg-gray-500 mb-4"></div>
-        {projects.map((item, index) => (
-          <div key={index}>
-            <div className="flex justify-evenly items-center flex-col sm:flex-row">
-              <div className="flex flex-row">
-                <div className="h-12 w-12 rounded-full bg-gray-300"></div>
-                <div className="flex flex-col ml-4 mr-4 mb-4">
-                  <span className="text-xl font-bold">{item.title}</span>
-                  <p className="text-gray-400">{item.description}</p>
+        {projects.length === 0 ? (
+          <div className="flex flex-col items-center justify-center">
+            <span className="text-xl font-semibold mb-4">
+              <Spinner color="emerald" />
+            </span>
+          </div>
+        ) : (
+          projects.map((item: ProjectProps, index: number) => (
+            <div key={index}>
+              <div className="flex justify-between items-center flex-col sm:flex-row">
+                <div className="flex flex-row">
+                  <Image
+                    src={item.imageUrl as string}
+                    alt="Imagem do projeto"
+                    className="h-12 w-12 rounded-full bg-gray-300 bg-center"
+                    width={300}
+                    height={300}
+                  />
+                  <div className="flex flex-col ml-4 mr-4 mb-4">
+                    <span className="text-xl font-bold">{item.title}</span>
+                    <p className="text-gray-400">{item.description}</p>
+                  </div>
+                </div>
+                <div className="flex flex-row gap-4">
+                  <button
+                    type="button"
+                    onClick={() =>
+                      handleEditClick(
+                        item.title,
+                        item.description,
+                        item.id,
+                        item?.imageUrl
+                      )
+                    }
+                  >
+                    <IoPencilOutline className="bg-gradient-to-r from-sky-500 to-blue-500 w-10 h-10 text-white rounded-md p-2.5 cursor-pointer" />
+                  </button>
+                  <IoTrashBinOutline className="bg-gradient-to-r from-red-500 to-orange-500 w-10 h-10 text-white rounded-md p-2.5 cursor-pointer" />
                 </div>
               </div>
-              <div className="flex flex-row gap-4">
-                <button
-                  type="button"
-                  onClick={() => {
-                    handleEditClick(item.title, item.description);
-                    handleModal();
-                  }}
-                >
-                  <IoPencilOutline className="bg-gradient-to-r from-sky-500 to-blue-500 w-10 h-10 text-white rounded-md p-2.5 cursor-pointer" />
-                </button>
-                <IoTrashBinOutline className="bg-gradient-to-r from-red-500 to-orange-500 w-10 h-10 text-white rounded-md p-2.5 cursor-pointer" />
-              </div>
-            </div>
-            {index !== 14 && (
               <div className="w-full border-b bg-gray-500 mb-4"></div>
-            )}
-          </div>
-        ))}
+            </div>
+          ))
+        )}
       </div>
 
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
-          <div className="bg-white p-6 rounded-xl z-10 w-96 modal">
+          <div className="bg-white p-6 rounded-xl z-10 w-96">
             <h2 className="text-2xl font-bold mb-4">Adicionar Projeto</h2>
             <div className="w-full border-b bg-gray-500 mb-4"></div>
             <Form
-              onSubmit={(data) => {
-                console.log(data);
-              }}
+              onSubmit={(data: ProjectProps) =>
+                onSubmit({ ...data, id: idProject })
+              }
             >
               <Input
                 label="Titulo"
@@ -146,9 +275,29 @@ export default function Home() {
                 className="mt-4"
                 defaultValue={editDescription}
               />
+              <InputFile
+                name="image"
+                onChange={(e) => {
+                  if (e.target.files) {
+                    setImage(e.target.files[0]);
+                  }
+                }}
+              />
               <div className="flex flex-row gap-4 mt-4 justify-between">
-                <button className="flex items-center gap-2 bg-gradient-to-r from-emerald-500 to-teal-500 text-white rounded-md p-2.5">
-                  <span className="text-md font-semibold">Cadastrar</span>
+                <button
+                  className="flex items-center gap-2 bg-gradient-to-r from-emerald-500 to-teal-500 text-white rounded-md p-2.5"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  <span className="text-md font-semibold">
+                    {isLoading ? (
+                      <Spinner color="emerald" />
+                    ) : editTitle || editDescription ? (
+                      "Atualizar"
+                    ) : (
+                      "Cadastrar"
+                    )}
+                  </span>
                 </button>
                 <button
                   className="flex items-center gap-2 bg-gradient-to-r from-red-500 to-orange-500 text-white rounded-md p-2.5 cursor-pointer"
@@ -159,6 +308,30 @@ export default function Home() {
                 </button>
               </div>
             </Form>
+            {editTitle && editDescription && !image && (
+              <div className="flex flex-col items-center mt-4">
+                <span className="text-md font-semibold">Imagem do projeto</span>
+                <Image
+                  src={imageURL as string}
+                  alt="Imagem do projeto"
+                  className="object-cover rounded-md mt-2"
+                  width={128}
+                  height={64}
+                />
+              </div>
+            )}
+            {image && (
+              <div className="flex flex-col items-center mt-4">
+                <span className="text-md font-semibold">Imagem do projeto</span>
+                <Image
+                  src={URL.createObjectURL(image)}
+                  alt="Imagem do projeto"
+                  className="object-cover rounded-md mt-2"
+                  width={128}
+                  height={64}
+                />
+              </div>
+            )}
           </div>
         </div>
       )}
--- src/app/dashboard/login/page.tsx
+++ src/app/dashboard/login/page.tsx
@@ -3,34 +3,10 @@
 import Form from "@/components/Atoms/Form";
 import { Input } from "@/components/Atoms/Input";
 import Logo from "@/components/Atoms/Logo";
+import { Spinner } from "@/components/Atoms/Spinner";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { IoMail, IoLockClosed } from "react-icons/io5";
-
-const Spinner = () => (
-  <div className="flex justify-center items-center">
-    <svg
-      className="animate-spin h-6 w-6 text-purple-500"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle
-        className="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        strokeWidth="4"
-      ></circle>
-      <path
-        className="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-      ></path>
-    </svg>
-  </div>
-);
 
 export default function Login() {
   const router = useRouter();
@@ -85,7 +61,7 @@ export default function Login() {
               className="p-3 rounded-3xl text-purple-500 bg-purple-500 bg-opacity-10 hover:bg-opacity-20 transition-all"
               disabled={isLoading}
             >
-              {isLoading ? <Spinner /> : "Entrar"}
+              {isLoading ? <Spinner color="purple" /> : "Entrar"}
             </button>
           </div>
         </Form>
